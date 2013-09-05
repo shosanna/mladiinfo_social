064532b44d2c677ff89abfc8683c724faa03cbec
Zuzana Dostalova <sanna23@icloud.com>
2013-09-05 23:47:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MladiinfoSocial::Application.config.secret_key_base = '701fafe4de8f836d0b5f6d952ce4bcc0890ce754738cc4452b1a69e3e085badb054f054e9454debda04154374aa79889c8cbb872e96e84df5ea019749eed9f6c'
