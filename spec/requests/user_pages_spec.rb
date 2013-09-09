09b83a287565dfcacef2e3bf1d067eaf302337e7
Zuzana Dostalova <sanna23@icloud.com>
2013-09-09 18:19:00 +0200

    nice tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,34 +17,30 @@ describe "User pages" do
 
     it { should have_content('Zaregistruj') }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
+    it "does not create a user with blank form" do
+      expect { click_button submit }.not_to change(User, :count)
     end
 
-    describe "with valid information" do
-      it "should create a user" do
-        fill_in "Username",     with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
-
-      it "shows flash after registration" do
-        fill_in "Username",     with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-        click_button submit
-        page.should have_selector('div.alert.alert-success')
-      end
+    it "creates a user with valid information" do
+      fill_in "Username",     with: "Example User"
+      fill_in "Email",        with: "user@example.com"
+      fill_in "Password",     with: "foobar"
+      fill_in "Confirmation", with: "foobar"
+      expect { click_button submit }.to change(User, :count).by(1)
     end
 
-    describe "after submission" do
-      before { click_button submit }
-      it { should have_content('error') }
+    it "shows flash after registration" do
+      fill_in "Username",     with: "Example User"
+      fill_in "Email",        with: "user@example.com"
+      fill_in "Password",     with: "foobar"
+      fill_in "Confirmation", with: "foobar"
+      click_button submit
+      page.should have_selector('div.alert.alert-success')
+    end
+
+    it "has error message when send without correct information" do
+      click_button submit
+      page.should have_content('error')
     end
   end
 end
