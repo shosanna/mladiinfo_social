e511ff72d311733f97bab217d8476d14699223c1
Zuzana Dostalova <sanna23@icloud.com>
2013-09-27 22:55:35 +0200

    workng sign in form in index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-   user = User.find_by(email: params[:session][:email].downcase)
+    Rails.logger.error(("\n" * 10) + params.inspect + ("\n" * 10))
+    user = User.find_by(email: params[:session][:email].downcase)
+
     if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_to user
